40c9d5b03d13e931cf5085069243c195089b0be5
Mikhaylov.nv2 <mikhaylov.nv2@dns-shop.ru>
2020-06-14 23:58:08 +1030

    Подключение MongoDB.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,34 @@
-import {Application, Request, Response, Router} from 'express';
+import {Application, Request, Response} from 'express';
+import {MongoClient, MongoError} from 'mongodb';
 import ServerConfig from '../config/config';
-import RoutesManager from '../modules/RoutesManages';
 import App from '../modules/App';
+import RoutesManager from '../modules/RoutesManages';
 
-const EXPRESS     = require('express');
-const BODY_PARSER = require('body-parser');
-const CORS        = require('cors');
-const MORGAN      = require('morgan');
-const config      = ServerConfig.getInstance();
+const EXPRESS                  = require('express');
+const BODY_PARSER              = require('body-parser');
+const cors                     = require('cors');
+const morgan                   = require('morgan');
+const config                   = ServerConfig.getInstance();
 
 const app: Application = App.get();
-const router: Router = Router();
 
-app.use(MORGAN('combined'));
-app.use(BODY_PARSER.json());
-app.use(CORS());
-
-app.listen(config.port, () => {
-	console.log('server is ok', config.port);
+const client = new MongoClient('mongodb://localhost:27017/', {
+	useNewUrlParser: true,
 });
+
+client.connect((error: MongoError, client: MongoClient) => {
+	if (error) {
+		console.log(error);
+	}
+
+	app.listen(config.port, () => {
+		console.log('server is ok', config.port);
+	});
+});
+
+app.use(morgan('combined'));
+app.use(BODY_PARSER.json());
+app.use(cors());
 
 app.use('/', EXPRESS.static(__dirname + '/dist'));
 
